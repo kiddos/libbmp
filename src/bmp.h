18e58effb064b864c5648c74171d5ede2eaf0e7f
Kiddos <josephyu831007@yahoo.com.tw>
2015-11-14 16:09:34 +0800

    add document for bit count field in info structure

--- src/bmp.h
+++ src/bmp.h
@@ -23,6 +23,8 @@ typedef unsigned int uint32_t;
 typedef unsigned int long32_t;
 #endif
 
+#define BITMAP_TYPE 19778
+
 typedef struct bitmap_file_header_t
 {
 	word_t _type;
@@ -59,6 +61,106 @@ typedef struct bitmap_t
 	void* data;
 } bitmap_t, bitmap;
 
+/***
+ *  BIT_COUNT meaning
+ *
+ *	reference:
+ *	https://msdn.microsoft.com/en-us/library/windows/desktop/dd183376(v=vs.85).aspx
+ *
+ *	for different values:
+ *
+ *	0:	The number of bits-per-pixel is specified or
+ *		is implied by the JPEG or PNG format.
+ *
+ *	1:	The bitmap is monochrome, and the bmiColors
+ *		member of BITMAPINFO contains two entries.
+ *		Each bit in the bitmap array represents a pixel.
+ *		If the bit is clear, the pixel is displayed
+ *		with the color of the first entry in the bmiColors
+ *		table; if the bit is set, the pixel has the
+ *		color of the second entry in the table.
+ *
+ *	4:	The bitmap has a maximum of 16 colors,
+ *		and the bmiColors member of BITMAPINFO
+ *		contains up to 16 entries. Each pixel in the
+ *		bitmap is represented by a 4-bit index
+ *		into the color table. For example, if the
+ *		first byte in the bitmap is 0x1F, the byte
+ *		represents two pixels. The first pixel
+ *		contains the color in the second table entry,
+ *		and the second pixel contains the color
+ *		in the sixteenth table entry.
+ *
+ *  8:	The bitmap has a maximum of 256 colors,
+ *		and the bmiColors member of BITMAPINFO
+ *		contains up to 256 entries. In this case,
+ *		each byte in the array represents a single pixel.
+ *
+ * 16:	The bitmap has a maximum of 2^16 colors.
+ *		If the biCompression member of the
+ *		BITMAPINFOHEADER is BI_RGB, the bmiColors
+ *		member of BITMAPINFO is NULL. Each WORD
+ *		in the bitmap array represents a single
+ *		pixel. The relative intensities of red, green,
+ *		and blue are represented with five bits for
+ *		each color component. The value for blue is
+ *		in the least significant five bits, followed
+ *		by five bits each for green and red.
+ *		The most significant bit is not used.
+ *		The bmiColors color table is used for
+ *		optimizing colors used on palette-based devices,
+ *		and must contain the number of entries
+ *		specified by the biClrUsed member of the
+ *		BITMAPINFOHEADER.
+ *		If the biCompression member of the
+ *		BITMAPINFOHEADER is BI_BITFIELDS, the
+ *		bmiColors member contains three DWORD color
+ *		masks that specify the red, green, and blue
+ *		components, respectively, of each pixel.
+ *		Each WORD in the bitmap array represents a
+ *		single pixel.
+ *		When the biCompression member is BI_BITFIELDS,
+ *		bits set in each DWORD mask must be contiguous
+ *		and should not overlap the bits of another mask.
+ *		All the bits in the pixel do not have to be used.
+ *
+ * 24:	The bitmap has a maximum of 2^24 colors,
+ *		and the bmiColors member of BITMAPINFO is NULL.
+ *		Each 3-byte triplet in the bitmap array
+ *		represents the relative intensities of blue,
+ *		green, and red, respectively, for a pixel.
+ *		The bmiColors color table is used for optimizing
+ *		colors used on palette-based devices, and must
+ *		contain the number of entries specified by the
+ *		biClrUsed member of the BITMAPINFOHEADER.
+ *
+ * 32:	The bitmap has a maximum of 2^32 colors.
+ *		If the biCompression member of the BITMAPINFOHEADER
+ *		is BI_RGB, the bmiColors member of BITMAPINFO
+ *		is NULL. Each DWORD in the bitmap array
+ *		represents the relative intensities of blue,
+ *		green, and red for a pixel. The value for blue
+ *		is in the least significant 8 bits, followed
+ *		by 8 bits each for green and red. The high
+ *		byte in each DWORD is not used. The bmiColors
+ *		color table is used for optimizing colors used
+ *		on palette-based devices, and must contain the
+ *		number of entries specified by the biClrUsed
+ *		member of the BITMAPINFOHEADER.
+ *
+ *		If the biCompression member of the BITMAPINFOHEADER
+ *		is BI_BITFIELDS, the bmiColors member contains
+ *		three DWORD color masks that specify the red,
+ *		green, and blue components, respectively,
+ *		of each pixel. Each DWORD in the bitmap array
+ *		represents a single pixel.
+ *
+ *		When the biCompression member is BI_BITFIELDS,
+ *		bits set in each DWORD mask must be contiguous and
+ *		should not overlap the bits of another mask.
+ *		All the bits in the pixel do not need to be used.
+ *
+ */
 typedef enum
 {
 	BITMAP_BIT_COUNT0    =   0,
@@ -90,7 +192,8 @@ void _bmp_write_info_header(bitmap_info_header_t* ih, FILE* file);
 void _bmp_print_info_header(bitmap_info_header_t* header);
 void _bmp_read_data(void* content, void* dst, size_t size);
 void _bmp_print_data(void* dst, size_t size);
-long_t _bmp_get_data_size(bitmap_file_header_t* ih);
+long_t _bmp_get_data_size(bitmap_file_header_t* fh);
+long_t _bmp_get_offset_size(bitmap_file_header_t* fh);
 
 long_t _bmp_get_image_bit_count(bitmap_t* bmp);
 long_t _bmp_get_image_x_pixels_per_meter(bitmap_t* bmp);
@@ -106,6 +209,7 @@ void bmp_get_image_info_header(bitmap_t* bmp, bitmap_info_header_t* ih);
 void bmp_get_image_data(bitmap_t* bmp, void* data, size_t data_size);
 
 bitmap_t* bmp_read(const char* bmp_name);
+bitmap_t* bmp_create(long_t width, long_t height, void* data, size_t size);
 void bmp_write(bitmap_t* bmp, const char* bmp_name);
 
 #endif /* end of include guard: BMP_H */
