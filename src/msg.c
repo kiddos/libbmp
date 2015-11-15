bd8f102e26f5dd7ad556f5c9ec149e4738378a88
Kiddos <josephyu831007@yahoo.com.tw>
2015-11-15 22:06:12 +0800

    fix bugs

--- src/msg.c
+++ src/msg.c
@@ -9,3 +9,29 @@ void msg_warning(const char* msg)
 {
 	fprintf(stderr, "WARNING: %s\n", msg);
 }
+
+void msg_error_line(const char* msg, unsigned int n, ...)
+{
+	va_list args;
+	va_start(args, n);
+	fprintf(stderr, "ERROR: %s", msg);
+
+	unsigned int i;
+	for (i = 0 ; i < n ; i++)
+		fprintf(stderr, " %s", va_arg(args, const char*));
+
+	va_end(args);
+}
+
+void msg_warning_line(const char* msg, unsigned int n, ...)
+{
+	va_list args;
+	va_start(args, n);
+	fprintf(stderr, "WARNING: %s", msg);
+
+	unsigned int i;
+	for (i = 0 ; i < n ; i++)
+		fprintf(stderr, " %s", va_arg(args, const char*));
+
+	va_end(args);
+}
