8d80c11459e04db9f227b503e1e3bfbe86d3a1a9
Kiddos <josephyu831007@yahoo.com.tw>
2015-11-15 22:05:57 +0800

    add a macro to check for null string and return

--- src/msg.h
+++ src/msg.h
@@ -2,8 +2,20 @@
 #define MSG_H
 
 #include <stdio.h>
+#include <stdarg.h>
+
+// macro check for null string and immediately return
+
+#define MSG_IS_NULL_STRING(str, status)       \
+	if (!str)                             \
+	{                                     \
+		msg_error("null string pointer"); \
+		return status;                    \
+	}                                     \
 
 void msg_error(const char* msg);
+void msg_error_line(const char* msg, unsigned int n, ...);
 void msg_warning(const char* msg);
+void msg_warning_line(const char* msg, unsigned int n, ...);
 
 #endif /* end of include guard: MSG_H */
